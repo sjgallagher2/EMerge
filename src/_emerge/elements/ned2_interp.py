changed extension of file storage and fixed a power normalization issue for ports

src/_emerge/elements/ned2_interp.py
@@ -558,7 +558,7 @@ def ned2_tri_interp_full(coords: np.ndarray,
         Ez[inside] = ez
     return Ex, Ey, Ez
 
-@njit(types.Tuple((c16[:], c16[:], c16[:]))(f8[:,:], c16[:], i8[:,:], f8[:,:], i8[:,:], c16[:,:,:], f8), cache=True, nogil=True)
+@njit(types.Tuple((c16[:], c16[:], c16[:]))(f8[:,:], c16[:], i8[:,:], f8[:,:], i8[:,:], c16[:,:,:], c16), cache=True, nogil=True)
 def ned2_tri_interp_curl(coords: np.ndarray,
                     solutions: np.ndarray, 
                     tris: np.ndarray,

src/_emerge/physics/microwave/microwave_3d.py
@@ -482,19 +482,20 @@ class Microwave3D:
             eigenmode = eigen_modes[:,i]
             Emode[solve_ids] = np.squeeze(eigenmode)
             Emode = Emode * np.exp(-1j*np.angle(np.max(Emode)))
-            beta = min(np.emath.sqrt(-eigen_values[i]).real, kmax.real)
+
+            beta = np.emath.sqrt(-eigen_values[i])
             
             residuals = -1
 
             portfE = nlf.interpolate_Ef(Emode)
-            portfH = nlf.interpolate_Hf(Emode, k0, ur, beta)
+            portfH = nlf.interpolate_Hf(Emode, k0, ur, beta.real)
 
             P = compute_avg_power_flux(nlf, Emode, k0, ur, beta)
 
             mode = port.add_mode(Emode, portfE, portfH, beta, k0, residuals, TEM=TEM, freq=freq)
             if mode is None:
                 continue
-            mode.set_power(P)
+            
 
             Ez = np.max(np.abs(Emode[nlf.n_xy:]))
             Exy = np.max(np.abs(Emode[:nlf.n_xy]))
@@ -513,6 +514,8 @@ class Microwave3D:
                 voltage = np.sum(Ex*dls[0,:] + Ey*dls[1,:] + Ez*dls[2,:])
                 mode.Z0 = voltage**2/(2*P)
                 logger.debug(f'Port Z0 = {mode.Z0}')
+
+            mode.set_power(P*port._qmode(k0)**2)
         
         port.sort_modes()
 

src/_emerge/physics/microwave/microwave_bc.py
@@ -163,6 +163,20 @@ class PortBC(RobinBC):
         else:
             return ValueError(f'Port mode type should be TEM, TE or TM but instead is {self.modetype(k0)}')
     
+    def _qmode(self, k0: float) -> float:
+        """Computes a mode amplitude correction factor.
+        The total output power of a port as a function of the field amplitude is not constant.
+        For TE and TM modes the output power depends on the mode impedance. This factor corrects
+        the mode output power to 1W by scaling the E-field appropriately.
+
+        Args:
+            k0 (float): The phase constant of the simulation
+
+        Returns:
+            float: The mode amplitude correction factor.
+        """
+        return np.sqrt(self.Zmode(k0)/376.73031341259)
+    
     @property
     def mode_number(self) -> int:
         return self.selected_mode + 1
@@ -453,9 +467,9 @@ class ModalPort(PortBC):
         This field-function is defined in global coordinates (not local coordinates).'''
         mode = self.get_mode(k0)
         if which == 'E':
-            return lambda x,y,z: mode.norm_factor*mode.E_function(x,y,z)*mode.polarity
+            return lambda x,y,z: mode.norm_factor * self._qmode(k0) * mode.E_function(x,y,z)*mode.polarity
         else:
-            return lambda x,y,z: mode.norm_factor*mode.H_function(x,y,z)*mode.polarity
+            return lambda x,y,z: mode.norm_factor * self._qmode(k0) * mode.H_function(x,y,z)*mode.polarity
     
     def clear_modes(self) -> None:
         """Clear all port mode data"""
@@ -542,7 +556,7 @@ class ModalPort(PortBC):
                             z_global: np.ndarray,
                             k0: float,
                             which: Literal['E','H'] = 'E') -> np.ndarray:
-        Ex, Ey, Ez = np.sqrt(self.power) * self.global_field_function(k0, which)(x_global,y_global,z_global)
+        Ex, Ey, Ez = self.global_field_function(k0, which)(x_global,y_global,z_global)
         Exyz = np.array([Ex, Ey, Ez])
         return Exyz
 
@@ -602,6 +616,9 @@ class RectangularWaveguide(PortBC):
     def portZ0(self, k0: float = None) -> complex:
         return k0*299792458 * 4*np.pi*1e-7/self.get_beta(k0)
 
+    def modetype(self, k0):
+        return self.type
+    
     def get_amplitude(self, k0: float) -> float:
         Zte = 376.73031341259
         amplitude= np.sqrt(self.power*4*Zte/(self.dims[0]*self.dims[1]))
@@ -624,7 +641,8 @@ class RectangularWaveguide(PortBC):
         ''' Return the out of plane propagation constant. βz.'''
         width=self.dims[0]
         height=self.dims[1]
-        return np.sqrt(k0**2 - (np.pi*self.mode[0]/width)**2 - (np.pi*self.mode[1]/height)**2)
+        beta = np.sqrt(k0**2 - (np.pi*self.mode[0]/width)**2 - (np.pi*self.mode[1]/height)**2)
+        return beta
     
     def get_gamma(self, k0: float):
         """Computes the γ-constant for matrix assembly. This constant is required for the Robin boundary condition.
@@ -654,7 +672,7 @@ class RectangularWaveguide(PortBC):
         Ex = 0*E
         Ey = E
         Ez = 0*E
-        Exyz = np.array([Ex, Ey, Ez])
+        Exyz =  self._qmode(k0) * np.array([Ex, Ey, Ez])
         return Exyz
 
     def port_mode_3d_global(self, 

src/_emerge/plot/pyvista/display.py
@@ -352,9 +352,6 @@ class PVDisplay(BaseDisplay):
             X = self._mesh.tri_centers[0,tris]
             Y = self._mesh.tri_centers[1,tris]
             Z = self._mesh.tri_centers[2,tris]
-            # for x,y,z in zip(X,Y,Z):
-            #     self.add_portmode(port, Npoints, dv, (x,y,z), field, k0=k0, mode_number=mode_number)
-            # return
         
         X = X+dv[0]
         Y = Y+dv[1]
@@ -395,9 +392,10 @@ class PVDisplay(BaseDisplay):
             F = np.real(F.T)
             Fnorm = np.sqrt(Fx.real**2 + Fy.real**2 + Fz.real**2).T
 
-        #grid = pv.StructuredGrid(X,Y,Z)
-        #self.add_surf(X,Y,Z,Fnorm, _fieldname = 'portfield')
-        #self._plot.add_mesh(grid, scalars = Fnorm, opacity=0.8, pickable=False)
+        if XYZ is not None:
+            grid = pv.StructuredGrid(X,Y,Z)
+            self.add_surf(X,Y,Z,Fnorm, _fieldname = 'portfield')
+            self._plot.add_mesh(grid, scalars = Fnorm.T, opacity=0.8, pickable=False)
 
         Emag = F/np.max(Fnorm.flatten())*d*3
         self._plot.add_arrows(np.array([xf,yf,zf]).T, Emag)