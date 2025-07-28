Improved SuperLU performance on larger models by almost 2x and fixen port power normalization bug

examples/demo1_stepped_imp_filter.py
@@ -135,7 +135,7 @@ plot_sp(f/1e9, [S11, S21], labels=['S11','S21'], dblim=[-40,6], logx=True)
 
 # We can also supersample our data by constructing a model using the Vector Fitting algorithm
 
-f = np.linspace(0.2e9, 10e9, 2001)
+f = np.linspace(0.2e9, 8e9, 2001)
 S11 = gritted_data.model_S(1,1,f)
 S21 = gritted_data.model_S(2,1,f)
 
@@ -145,5 +145,5 @@ plot_sp(f/1e9, [S11, S21], labels=['S11','S21'], dblim=[-40,6], logx=True)
 
 m.display.add_object(pcb, opacity=0.1)
 m.display.add_object(polies, opacity=0.5)
-m.display.add_surf(*sol.field.find(freq=1e9).cutplane(1*mm, z=-0.75*th*mil).scalar('Ez','real'))
+m.display.add_surf(*sol.field[0].cutplane(1*mm, z=-0.75*th*mil).scalar('Ez','real'))
 m.display.show()

src/_emerge/elements/nedleg2.py
@@ -130,7 +130,7 @@ class NedelecLegrange2(FEMBasis):
         self.tri_to_field[8:11,:] = self.mesh.tris + (2*Ne + 2*Nt) # + E + T + E + T
         self.tri_to_field[11:14,:] = self.mesh.tri_to_edge + (2*Ne + 2*Nt + Nn)
     
-        self.edge_to_field: np.ndarray = np.zeros((5,Ne), dtype=int)
+        self.edge_to_field: np.ndarray = np.zeros((5,Ne), dtype=int) #edge mode 1, edge mode 2, edge legrande mode, edge vertex mode 1, edge vertex mode 2
 
         self.edge_to_field[0,:] = np.arange(Ne)
         self.edge_to_field[1,:] = np.arange(Ne) + Nt + Ne

src/_emerge/physics/microwave/assembly/assembler.py
@@ -133,9 +133,6 @@ class Assembler:
         ermesh = ermesh - 1j * sigmesh/(k0*C0*EPS0)
 
         E, B = generelized_eigenvalue_matrix(nedlegfield, ermesh, urmesh, port.cs._basis, k0)
-        
-
-       
 
         pecs: list[PEC] = [bc for bc in bcs if isinstance(bc,PEC)]
         if len(pecs) > 0:
@@ -156,7 +153,6 @@ class Assembler:
 
             tri_ids = mesh.get_triangles(face_tags)
             edge_ids = list(mesh.tri_to_edge[:,tri_ids].flatten())
-            
             for ii in edge_ids:
                 i2 = nedlegfield.mesh.from_source_edge(ii)
                 if i2 is None:
@@ -166,7 +162,7 @@ class Assembler:
                 pec_edges.append(eids[0])
                 pec_vertices.append(eids[3]-nedlegfield.n_xy)
                 pec_vertices.append(eids[4]-nedlegfield.n_xy)
-                
+            
 
             for ii in tri_ids:
                 i2 = nedlegfield.mesh.from_source_tri(ii)
@@ -182,8 +178,6 @@ class Assembler:
         pec_ids = set(pec_ids)
         solve_ids = [i for i in range(nedlegfield.n_field) if i not in pec_ids]
 
-        #matplot(E, solve_ids)
-        #matplot(B, solve_ids)
         return E, B, np.array(solve_ids), nedlegfield
 
     def assemble_freq_matrix(self, field: Nedelec2, 

src/_emerge/physics/microwave/assembly/generalized_eigen.py
@@ -43,7 +43,7 @@ def generelized_eigenvalue_matrix(field: NedelecLegrange2,
     nodes = np.linalg.pinv(basis) @ nodes
     
     with ProgressBar(total=nT, ncols=100, dynamic_ncols=False) as pgb:
-        dataE, dataB, rows, cols = _matrix_builder(nodes, tris, edges, tri_to_field, ur, er,k0, pgb)
+        dataE, dataB, rows, cols = _matrix_builder(nodes, tris, edges, tri_to_field, ur, er, k0, pgb)
     
     nfield = field.n_field
 
@@ -157,7 +157,7 @@ def _ne1_curl(coeff, coords):
     a2, b2, c2 = coeff[:,1]
     xs = coords[0,:]
     ys = coords[1,:]
-    return -3*a1*b1*c2 + 3*a1*b2*c1 - 3*b1**2*c2*xs + 3*b1*b2*c1*xs - 3*b1*c1*c2*ys + 3*b2*c1**2*ys + 0*1j
+    return -3*a1*b1*c2 + 3*a1*b2*c1 - 3*b1**2*c2*xs + 3*b1*b2*c1*xs - 3*b1*c1*c2*ys + 3*b2*c1**2*ys + 0j
 
 @njit(c16[:](f8[:,:], f8[:,:]), cache=True, nogil=True)
 def _ne2_curl(coeff, coords):
@@ -165,7 +165,7 @@ def _ne2_curl(coeff, coords):
     a2, b2, c2 = coeff[:,1]
     xs = coords[0,:]
     ys = coords[1,:]
-    return -3*a2*b1*c2 + 3*a2*b2*c1 - 3*b1*b2*c2*xs - 3*b1*c2**2*ys + 3*b2**2*c1*xs + 3*b2*c1*c2*ys+ 0*1j
+    return -3*a2*b1*c2 + 3*a2*b2*c1 - 3*b1*b2*c2*xs - 3*b1*c2**2*ys + 3*b2**2*c1*xs + 3*b2*c1*c2*ys+ 0j
 
 @njit(c16[:](f8[:,:], f8[:,:]), cache=True, nogil=True)
 def _nf1_curl(coeff, coords):
@@ -269,93 +269,103 @@ def generalized_matrix_GQ(tri_vertices, local_edge_map, Ms, Mm, k0):
     Ls[:,3] *= Ds[0,2]
     Ls[:,7] *= Ds[0,1]
 
-    for ei in range(3):
-        eis = local_edge_map[:, ei]
+    for iv1 in range(3):
+        ie1 = local_edge_map[:, iv1]
         
-        Le = Ds[eis[0], eis[1]]
-        Ls[ei,:] *= Le
-        Ls[:,ei] *= Le
-        Ls[ei+4,:] *= Le
-        Ls[:,ei+4] *= Le
+        Le = Ds[ie1[0], ie1[1]]
+        Ls[iv1,:] *= Le
+        Ls[:,iv1] *= Le
+        Ls[iv1+4,:] *= Le
+        Ls[:,iv1+4] *= Le
+        F1 = _ne1_curl(coeff[:,ie1], cs)
+        F2 = _ne2_curl(coeff[:,ie1], cs)
+        F3 = _ne1(coeff[:,ie1], cs)
+        F4 = _ne2(coeff[:,ie1], cs)
+        F5 = _lv_grad(coeff[:,iv1],cs)
+        F6 = _le_grad(coeff[:,ie1],cs)
+        for iv2 in range(3):
+            ei2 = local_edge_map[:, iv2]
+            
+            
+            Att[iv1,iv2]     = _gqi(F1, Msz * _ne1_curl(coeff[:,ei2],cs), WEIGHTS)
+            Att[iv1+4,iv2]   = _gqi(F2, Msz * _ne1_curl(coeff[:,ei2],cs), WEIGHTS)
+            Att[iv1,iv2+4]   = _gqi(F1, Msz * _ne2_curl(coeff[:,ei2],cs), WEIGHTS)
+            Att[iv1+4,iv2+4] = _gqi(F2, Msz * _ne2_curl(coeff[:,ei2],cs), WEIGHTS)
+
+            Btt[iv1,iv2]     = _gqi2(F3, matmul(Mm,_ne1(coeff[:,ei2],cs)), WEIGHTS)
+            Btt[iv1+4,iv2]   = _gqi2(F4, matmul(Mm,_ne1(coeff[:,ei2],cs)), WEIGHTS)
+            Btt[iv1,iv2+4]   = _gqi2(F3, matmul(Mm,_ne2(coeff[:,ei2],cs)), WEIGHTS)
+            Btt[iv1+4,iv2+4] = _gqi2(F4, matmul(Mm,_ne2(coeff[:,ei2],cs)), WEIGHTS)
+
+            Dtt[iv1,iv2]     = _gqi2(F3, matmul(Ms,_ne1(coeff[:,ei2],cs)), WEIGHTS)
+            Dtt[iv1+4,iv2]   = _gqi2(F4, matmul(Ms,_ne1(coeff[:,ei2],cs)), WEIGHTS)
+            Dtt[iv1,iv2+4]   = _gqi2(F3, matmul(Ms,_ne2(coeff[:,ei2],cs)), WEIGHTS)
+            Dtt[iv1+4,iv2+4] = _gqi2(F4, matmul(Ms,_ne2(coeff[:,ei2],cs)), WEIGHTS)
+
+            Dzt[iv1, iv2]     = _gqi2(F5, matmul(Ms,_ne1(coeff[:,ei2],cs)), WEIGHTS)
+            Dzt[iv1+3, iv2]   = _gqi2(F6, matmul(Ms,_ne1(coeff[:,ei2],cs)), WEIGHTS)
+            Dzt[iv1, iv2+4]   = _gqi2(F5, matmul(Ms,_ne2(coeff[:,ei2],cs)), WEIGHTS)
+            Dzt[iv1+3, iv2+4] = _gqi2(F6, matmul(Ms,_ne2(coeff[:,ei2],cs)), WEIGHTS)
+
+            Dzz1[iv1, iv2]     = _gqi2(_lv_grad(coeff[:,iv1], cs), matmul(Ms,_lv_grad(coeff[:,iv2],cs)), WEIGHTS)
+            Dzz1[iv1, iv2+3]   = _gqi2(_lv_grad(coeff[:,iv1], cs), matmul(Ms,_le_grad(coeff[:,ei2],cs)), WEIGHTS)
+            Dzz1[iv1+3, iv2]   = _gqi2(_le_grad(coeff[:,ie1], cs), matmul(Ms,_lv_grad(coeff[:,iv2],cs)), WEIGHTS)
+            Dzz1[iv1+3, iv2+3] = _gqi2(_le_grad(coeff[:,ie1], cs), matmul(Ms,_le_grad(coeff[:,ei2],cs)), WEIGHTS)
+
+            Dzz2[iv1, iv2]     = _gqi(_lv(coeff[:,iv1], cs), Mmz * _lv(coeff[:,iv2],cs), WEIGHTS)
+            Dzz2[iv1, iv2+3]   = _gqi(_lv(coeff[:,iv1], cs), Mmz * _le(coeff[:,ei2],cs), WEIGHTS)
+            Dzz2[iv1+3, iv2]   = _gqi(_le(coeff[:,ie1], cs), Mmz * _lv(coeff[:,iv2],cs), WEIGHTS)
+            Dzz2[iv1+3, iv2+3] = _gqi(_le(coeff[:,ie1], cs), Mmz * _le(coeff[:,ei2],cs), WEIGHTS)
+
+
+        G1 = matmul(Mm,_nf1(coeff,cs))
+        G2 = matmul(Mm,_nf2(coeff,cs))
+        G3 = matmul(Ms,_nf1(coeff,cs))
+        G4 = matmul(Ms,_nf2(coeff,cs))
+        Att[iv1,3]   = _gqi(F1, Msz * _nf1_curl(coeff,cs), WEIGHTS)
+        Att[iv1+4,3] = _gqi(_ne2_curl(coeff[:,ie1], cs), Msz * _nf1_curl(coeff,cs), WEIGHTS)
+        Att[iv1,7]   = _gqi(F1, Msz * _nf2_curl(coeff,cs), WEIGHTS)
+        Att[iv1+4,7] = _gqi(_ne2_curl(coeff[:,ie1], cs), Msz * _nf2_curl(coeff,cs), WEIGHTS)
         
-        for ej in range(3):
-            ejs = local_edge_map[:, ej]
+        Att[3, iv1]   = Att[iv1,3]
+        Att[7, iv1]   = Att[iv1,7]
+        Att[3, iv1+4] = Att[iv1+4,3]
+        Att[7, iv1+4] = Att[iv1+4,7]
 
-            Att[ei,ej]     = _gqi(_ne1_curl(coeff[:,eis], cs), Msz * _ne1_curl(coeff[:,ejs],cs), WEIGHTS)
-            Att[ei+4,ej]   = _gqi(_ne2_curl(coeff[:,eis], cs), Msz * _ne1_curl(coeff[:,ejs],cs), WEIGHTS)
-            Att[ei,ej+4]   = _gqi(_ne1_curl(coeff[:,eis], cs), Msz * _ne2_curl(coeff[:,ejs],cs), WEIGHTS)
-            Att[ei+4,ej+4] = _gqi(_ne2_curl(coeff[:,eis], cs), Msz * _ne2_curl(coeff[:,ejs],cs), WEIGHTS)
+        Btt[iv1,3]   = _gqi2(F3, G1, WEIGHTS)
+        Btt[iv1+4,3] = _gqi2(F4, G1, WEIGHTS)
+        Btt[iv1,7]   = _gqi2(F3, G2, WEIGHTS)
+        Btt[iv1+4,7] = _gqi2(F4, G2, WEIGHTS)
 
-            Btt[ei,ej]     = _gqi2(_ne1(coeff[:,eis], cs), matmul(Mm,_ne1(coeff[:,ejs],cs)), WEIGHTS)
-            Btt[ei+4,ej]   = _gqi2(_ne2(coeff[:,eis], cs), matmul(Mm,_ne1(coeff[:,ejs],cs)), WEIGHTS)
-            Btt[ei,ej+4]   = _gqi2(_ne1(coeff[:,eis], cs), matmul(Mm,_ne2(coeff[:,ejs],cs)), WEIGHTS)
-            Btt[ei+4,ej+4] = _gqi2(_ne2(coeff[:,eis], cs), matmul(Mm,_ne2(coeff[:,ejs],cs)), WEIGHTS)
+        Btt[3, iv1]   = Btt[iv1,3]
+        Btt[7, iv1]   = Btt[iv1,7]
+        Btt[3, iv1+4] = Btt[iv1+4,3]
+        Btt[7, iv1+4] = Btt[iv1+4,7]
 
-            Dtt[ei,ej]     = _gqi2(_ne1(coeff[:,eis], cs), matmul(Ms,_ne1(coeff[:,ejs],cs)), WEIGHTS)
-            Dtt[ei+4,ej]   = _gqi2(_ne2(coeff[:,eis], cs), matmul(Ms,_ne1(coeff[:,ejs],cs)), WEIGHTS)
-            Dtt[ei,ej+4]   = _gqi2(_ne1(coeff[:,eis], cs), matmul(Ms,_ne2(coeff[:,ejs],cs)), WEIGHTS)
-            Dtt[ei+4,ej+4] = _gqi2(_ne2(coeff[:,eis], cs), matmul(Ms,_ne2(coeff[:,ejs],cs)), WEIGHTS)
+        Dtt[iv1,3]   = _gqi2(F3, G3, WEIGHTS)
+        Dtt[iv1+4,3] = _gqi2(F4, G3, WEIGHTS)
+        Dtt[iv1,7]   = _gqi2(F3, G4, WEIGHTS)
+        Dtt[iv1+4,7] = _gqi2(F4, G4, WEIGHTS)
 
-            Dzt[ei, ej]     = _gqi2(_lv_grad(coeff[:,ei],cs), matmul(Ms,_ne1(coeff[:,ejs],cs)), WEIGHTS)
-            Dzt[ei, ej+4]   = _gqi2(_lv_grad(coeff[:,ei],cs), matmul(Ms,_ne2(coeff[:,ejs],cs)), WEIGHTS)
-            Dzt[ei+3, ej]   = _gqi2(_le_grad(coeff[:,eis],cs), matmul(Ms,_ne1(coeff[:,ejs],cs)), WEIGHTS)
-            Dzt[ei+3, ej+4] = _gqi2(_le_grad(coeff[:,eis],cs), matmul(Ms,_ne2(coeff[:,ejs],cs)), WEIGHTS)
+        Dtt[3, iv1]   = Dtt[iv1,3]
+        Dtt[7, iv1]   = Dtt[iv1,7]
+        Dtt[3, iv1+4] = Dtt[iv1+4,3]
+        Dtt[7, iv1+4] = Dtt[iv1+4,7]
 
-            Dzz1[ei, ej]     = _gqi2(_lv_grad(coeff[:,ei], cs), matmul(Ms,_lv_grad(coeff[:,ej],cs)), WEIGHTS)
-            Dzz1[ei, ej+3]   = _gqi2(_lv_grad(coeff[:,ei], cs), matmul(Ms,_le_grad(coeff[:,ejs],cs)), WEIGHTS)
-            Dzz1[ei+3, ej]   = _gqi2(_le_grad(coeff[:,eis], cs), matmul(Ms,_lv_grad(coeff[:,ej],cs)), WEIGHTS)
-            Dzz1[ei+3, ej+3] = _gqi2(_le_grad(coeff[:,eis], cs), matmul(Ms,_le_grad(coeff[:,ejs],cs)), WEIGHTS)
+        Dzt[iv1, 3]   = _gqi2(F5, G3, WEIGHTS)
+        Dzt[iv1, 7]   = _gqi2(F5, G4, WEIGHTS)
+        Dzt[iv1+3, 3] = _gqi2(F6, G3, WEIGHTS)
+        Dzt[iv1+3, 7] = _gqi2(F6, G4, WEIGHTS)
 
-            Dzz2[ei, ej]     = _gqi(_lv(coeff[:,ei], cs), Mmz * _lv(coeff[:,ej],cs), WEIGHTS)
-            Dzz2[ei, ej+3]   = _gqi(_lv(coeff[:,ei], cs), Mmz * _le(coeff[:,ejs],cs), WEIGHTS)
-            Dzz2[ei+3, ej]   = _gqi(_le(coeff[:,eis], cs), Mmz * _lv(coeff[:,ej],cs), WEIGHTS)
-            Dzz2[ei+3, ej+3] = _gqi(_le(coeff[:,eis], cs), Mmz * _le(coeff[:,ejs],cs), WEIGHTS)
+    Att[3,3] = _gqi(_nf1_curl(coeff, cs), Msz * _nf1_curl(coeff,cs), WEIGHTS)
+    Att[7,3] = _gqi(_nf2_curl(coeff, cs), Msz * _nf1_curl(coeff,cs), WEIGHTS)
+    Att[3,7] = _gqi(_nf1_curl(coeff, cs), Msz * _nf2_curl(coeff,cs), WEIGHTS)
+    Att[7,7] = _gqi(_nf2_curl(coeff, cs), Msz * _nf2_curl(coeff,cs), WEIGHTS)
 
-
-        Att[ei,3]   = _gqi(_ne1_curl(coeff[:,eis], cs), Msz * _nf1_curl(coeff[:,fid],cs), WEIGHTS)
-        Att[ei+4,3] = _gqi(_ne2_curl(coeff[:,eis], cs), Msz * _nf1_curl(coeff[:,fid],cs), WEIGHTS)
-        Att[ei,7]   = _gqi(_ne1_curl(coeff[:,eis], cs), Msz * _nf2_curl(coeff[:,fid],cs), WEIGHTS)
-        Att[ei+4,7] = _gqi(_ne2_curl(coeff[:,eis], cs), Msz * _nf2_curl(coeff[:,fid],cs), WEIGHTS)
-        
-        Att[3, ei]   = Att[ei,3]
-        Att[7, ei]   = Att[ei,7]
-        Att[3, ei+4] = Att[ei+4,3]
-        Att[7, ei+4] = Att[ei+4,7]
-
-        Btt[ei,3]   = _gqi2(_ne1(coeff[:,eis], cs), matmul(Mm,_nf1(coeff[:,fid],cs)), WEIGHTS)
-        Btt[ei+4,3] = _gqi2(_ne2(coeff[:,eis], cs), matmul(Mm,_nf1(coeff[:,fid],cs)), WEIGHTS)
-        Btt[ei,7]   = _gqi2(_ne1(coeff[:,eis], cs), matmul(Mm,_nf2(coeff[:,fid],cs)), WEIGHTS)
-        Btt[ei+4,7] = _gqi2(_ne2(coeff[:,eis], cs), matmul(Mm,_nf2(coeff[:,fid],cs)), WEIGHTS)
-
-        Btt[3, ei]   = Btt[ei,3]
-        Btt[7, ei]   = Btt[ei,7]
-        Btt[3, ei+4] = Btt[ei+4,3]
-        Btt[7, ei+4] = Btt[ei+4,7]
-
-        Dtt[ei,3]   = _gqi2(_ne1(coeff[:,eis], cs), matmul(Ms,_nf1(coeff[:,fid],cs)), WEIGHTS)
-        Dtt[ei+4,3] = _gqi2(_ne2(coeff[:,eis], cs), matmul(Ms,_nf1(coeff[:,fid],cs)), WEIGHTS)
-        Dtt[ei,7]   = _gqi2(_ne1(coeff[:,eis], cs), matmul(Ms,_nf2(coeff[:,fid],cs)), WEIGHTS)
-        Dtt[ei+4,7] = _gqi2(_ne2(coeff[:,eis], cs), matmul(Ms,_nf2(coeff[:,fid],cs)), WEIGHTS)
-
-        Dtt[3, ei]   = Dtt[ei,3]
-        Dtt[7, ei]   = Dtt[ei,7]
-        Dtt[3, ei+4] = Dtt[ei+4,3]
-        Dtt[7, ei+4] = Dtt[ei+4,7]
-
-        Dzt[ei, 3]   = _gqi2(_lv_grad(coeff[:,ei],cs), matmul(Ms,_nf1(coeff[:,fid],cs)), WEIGHTS)
-        Dzt[ei, 7]   = _gqi2(_lv_grad(coeff[:,ei],cs), matmul(Ms,_nf2(coeff[:,fid],cs)), WEIGHTS)
-        Dzt[ei+3, 3] = _gqi2(_le_grad(coeff[:,eis],cs), matmul(Ms,_nf1(coeff[:,fid],cs)), WEIGHTS)
-        Dzt[ei+3, 7] = _gqi2(_le_grad(coeff[:,eis],cs), matmul(Ms,_nf2(coeff[:,fid],cs)), WEIGHTS)
-
-    Att[3,3] = _gqi(_nf1_curl(coeff[:,fid], cs), Msz * _nf1_curl(coeff[:,fid],cs), WEIGHTS)
-    Att[7,3] = _gqi(_nf2_curl(coeff[:,fid], cs), Msz * _nf1_curl(coeff[:,fid],cs), WEIGHTS)
-    Att[3,7] = _gqi(_nf1_curl(coeff[:,fid], cs), Msz * _nf2_curl(coeff[:,fid],cs), WEIGHTS)
-    Att[7,7] = _gqi(_nf2_curl(coeff[:,fid], cs), Msz * _nf2_curl(coeff[:,fid],cs), WEIGHTS)
-
-    Btt[3,3] = _gqi2(_nf1(coeff[:,fid], cs), matmul(Mm,_nf1(coeff[:,fid],cs)), WEIGHTS)
-    Btt[7,3] = _gqi2(_nf2(coeff[:,fid], cs), matmul(Mm,_nf1(coeff[:,fid],cs)), WEIGHTS)
-    Btt[3,7] = _gqi2(_nf1(coeff[:,fid], cs), matmul(Mm,_nf2(coeff[:,fid],cs)), WEIGHTS)
-    Btt[7,7] = _gqi2(_nf2(coeff[:,fid], cs), matmul(Mm,_nf2(coeff[:,fid],cs)), WEIGHTS)
+    Btt[3,3] = _gqi2(_nf1(coeff, cs), G1, WEIGHTS)
+    Btt[7,3] = _gqi2(_nf2(coeff, cs), G1, WEIGHTS)
+    Btt[3,7] = _gqi2(_nf1(coeff, cs), G2, WEIGHTS)
+    Btt[7,7] = _gqi2(_nf2(coeff, cs), G2, WEIGHTS)
 
     A = np.zeros((14, 14), dtype = np.complex128)
     B = np.zeros((14, 14), dtype = np.complex128)
@@ -409,7 +419,7 @@ def _matrix_builder(nodes, tris, edges, tri_to_field, ur, er, k0, pgb: ProgressB
         indices = tri_to_field[:, itri]
         for ii in range(14):
             rows[p+14*ii:p+14*(ii+1)] = indices[ii]
-            cols[p+ii:p+196:14] = indices[ii]
+            cols[p+ii:p+ii+196:14] = indices[ii]
 
         dataE[p:p+196] = Esub.ravel()
         dataB[p:p+196] = Bsub.ravel()

src/_emerge/physics/microwave/microwave_3d.py
@@ -231,7 +231,7 @@ class Microwave3D:
             Callable: The discretizer function
         """
         def disc(material: Material):
-            return 299792456/(max(self.frequencies) * np.real(material.neff))
+            return 299792458/(max(self.frequencies) * np.real(material.neff))
         return disc
     
     def _initialize_field(self):
@@ -483,12 +483,12 @@ class Microwave3D:
             Emode[solve_ids] = np.squeeze(eigenmode)
             Emode = Emode * np.exp(-1j*np.angle(np.max(Emode)))
 
+            #beta = min(k0*np.sqrt(ermax*urmax), np.emath.sqrt(-eigen_values[i]))
             beta = np.emath.sqrt(-eigen_values[i])
-            
             residuals = -1
 
             portfE = nlf.interpolate_Ef(Emode)
-            portfH = nlf.interpolate_Hf(Emode, k0, ur, beta.real)
+            portfH = nlf.interpolate_Hf(Emode, k0, ur, beta)
 
             P = compute_avg_power_flux(nlf, Emode, k0, ur, beta)
 
@@ -496,20 +496,21 @@ class Microwave3D:
             if mode is None:
                 continue
             
+            Efxy = Emode[:nlf.n_xy]
+            Efz = Emode[nlf.n_xy:]
+            Ez = np.max(np.abs(Efz))
+            Exy = np.max(np.abs(Efxy))
 
-            Ez = np.max(np.abs(Emode[nlf.n_xy:]))
-            Exy = np.max(np.abs(Emode[:nlf.n_xy]))
-
-            if Ez/Exy < 1e-5 and not TEM:
+            if Ez/Exy < 1e-3 and not TEM:
                 logger.debug('Low Ez/Et ratio detected, assuming TE mode')
                 mode.modetype = 'TE'
-            elif Ez/Exy > 1e-5 and not TEM:
+            elif Ez/Exy > 1e-3 and not TEM:
                 logger.debug('High Ez/Et ratio detected, assuming TM mode')
                 mode.modetype = 'TM'
             elif TEM:
                 G1, G2 = self._find_tem_conductors(port, sigtri=cond)
                 cs, dls = self._compute_integration_line(G1,G2)
-                
+                mode.modetype='TEM'
                 Ex, Ey, Ez = portfE(cs[0,:], cs[1,:], cs[2,:])
                 voltage = np.sum(Ex*dls[0,:] + Ey*dls[1,:] + Ez*dls[2,:])
                 mode.Z0 = voltage**2/(2*P)
@@ -620,6 +621,9 @@ class Microwave3D:
 
         ## Single threaded
         job_id = 1
+
+        self._compute_modes(sum(self.frequencies)/len(self.frequencies))
+
         if not parallel:
             # ITERATE OVER FREQUENCIES
             freq_groups

src/_emerge/physics/microwave/microwave_bc.py
@@ -786,10 +786,10 @@ class LumpedPort(PortBC):
         Returns:
             complex: The γ-constant
         """
-        return 1j*k0*376.7303/self.surfZ
+        return 1j*k0*376.730313412/self.surfZ
     
     def get_Uinc(self, x_local, y_local, k0) -> np.ndarray:
-        Emag = -1j*2*k0 * self.voltage/self.height * (376.7303/self.surfZ)
+        Emag = -1j*2*k0 * self.voltage/self.height * (376.730313412/self.surfZ)
         return Emag*self.port_mode_3d(x_local, y_local, k0)
     
     def port_mode_3d(self, 
@@ -915,4 +915,4 @@ class LumpedElement(RobinBC):
         Returns:
             complex: The γ-constant
         """
-        return 1j*k0*376.7303/self.surfZ(k0)
+        return 1j*k0*376.730313412/self.surfZ(k0)

src/_emerge/physics/microwave/microwave_data.py
@@ -297,7 +297,7 @@ class FarfieldData:
             logger.warning('Defaulting to normE')
             F = np.sqrt(np.abs(self.E[0,:])**2 + np.abs(self.E[1,:])**2 + np.abs(self.E[2,:])**2)
         if isotropic:
-            F = F/np.sqrt(376/(2*np.pi))
+            F = F/np.sqrt(376.730313412/(2*np.pi))
         if dB:
             F = 20*np.log10(np.clip(np.abs(F), a_min=10**(dBfloor/20), a_max = 1e9))-dBfloor
         if rmax is not None:

src/_emerge/solver.py
@@ -351,13 +351,13 @@ class SolverSuperLU(Solver):
         self.A: np.ndarray = None
         self.b: np.ndarray = None
         self._perm_c = None
-        self.options: dict[str, str] = dict(SymmetricMode=True)
+        self.options: dict[str, str] = dict(SymmetricMode=True, Equil=False, IterRefine='SINGLE')
         self.lu = None
     def solve(self, A, b, precon, reuse_factorization: bool = False, id: int = -1):
         logger.info(f'Calling SuperLU Solver, ID={id}')
         self.single = True
         if not reuse_factorization:
-            self.lu = splu(A, permc_spec='MMD_AT_PLUS_A', diag_pivot_thresh=0.001, options=self.options)
+            self.lu = splu(A, permc_spec='MMD_AT_PLUS_A', relax=2, diag_pivot_thresh=0.001, options=self.options)
         x = self.lu.solve(b)
         return x, 0
 
@@ -432,7 +432,7 @@ class SolverLAPACK(Solver):
         vecs : (n, m) complex ndarray eigenvectors, B-orthonormal  (xiᴴ B xj = δij)
         """
         logger.debug('Calling LAPACK eig solver')
-        lam, vecs = eig(A.toarray(), B.toarray(), right=True, left=False)
+        lam, vecs = eig(A.toarray(), B.toarray(), overwrite_a=True, overwrite_b=True, check_finite=False)
         lam, vecs = filter_real_modes(lam, vecs, target_k0, 2, 2)
         return lam, vecs
     